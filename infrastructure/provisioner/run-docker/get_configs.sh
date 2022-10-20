28271dc50fabcebb9c3983b7e46f01837e24f2c2
Long Tran <longhtran91@gmail.com>
2022-10-20 10:14:19 -0500

    add missing key param

--- infrastructure/provisioner/run-docker/get_configs.sh
+++ infrastructure/provisioner/run-docker/get_configs.sh
@@ -17,6 +17,7 @@ cat << EOF > ~/gatewise/secrets/web/app.env
 REFRESH_TOKEN=$(aws secretsmanager get-secret-value --secret-id gatewise/web --query SecretString --output=text | jq -r '.refresh_token')
 USER_AGENT=$(aws secretsmanager get-secret-value --secret-id gatewise/web --query SecretString --output=text | jq -r '.user_agent')
 DEVICE_TOKEN=$(aws secretsmanager get-secret-value --secret-id gatewise/web --query SecretString --output=text | jq -r '.device_token')
+KEY=$(aws secretsmanager get-secret-value --secret-id gatewise/web --query SecretString --output=text | jq -r '.key')
 EOF
 
 # set proper permissions for secrets 700 for dir and 600 for files
