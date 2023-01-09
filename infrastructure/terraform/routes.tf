3062c04d4be70399a38f351cf43ded739089cc37
Long Tran <longhtran91@gmail.com>
2023-01-08 21:17:51 -0600

    test add route rules

--- infrastructure/terraform/routes.tf
+++ infrastructure/terraform/routes.tf
@@ -16,14 +16,14 @@ resource "oci_core_route_table" "home_vpn_routes" {
     route_rules {
         network_entity_id = data.oci_core_private_ips.ocipl_app_gatewise.private_ips[0].id
         destination = "1.1.1.1/32"
-        destination_type = "CIRD_BLOCK"
+        destination_type = "CIDR_BLOCK"
     }
 
     # dynamic "route_rules"  {
     #   for_each = var.home_vpn_cidrs
     #   content {
     #     network_entity_id = data.oci_core_private_ips.ocipl_app_gatewise.id
-    #     destination_type = "CIRD_BLOCK"
+    #     destination_type = "CIDR_BLOCK"
     #     destination = route_rules.value
     #   }
     # }
